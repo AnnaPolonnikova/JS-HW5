f3972569118048b0a5923c78e1b1ba606a73dca4
Anna <Anna.polonnikova@list.ru>
2022-07-20 21:31:23 +0300

    commit2

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,4 +2,13 @@
 function test (a, b, c){
     return a + b - c;
 }
-console.log (test(3 , 5, 2))
+console.log (test(3 , 5, 2));
+// Задание2
+function  test2 (numb){
+    var square = numb * numb;
+    var cube = numb * numb * numb;
+    console.log (square);
+console.log (cube);
+}
+test2 (2)
+
